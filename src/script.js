1c05e8c1c5f7c31ded284ff8f36e052df7abe011
nblaaa <nblaaa@mail.ru>
2022-06-20 18:50:04 +0700

    Add basket

--- src/script.js
+++ src/script.js
@@ -1,12 +1,36 @@
 const basket = 'basket';
 
-localStorage.setItem(basket, JSON.stringify([]));
+const addItem = (name, objectID) => {
+    const prevBasket = localStorage.getItem(basket) ? JSON.parse(localStorage.getItem(basket)) : [];
+    const index = prevBasket.findIndex(item => item.objectID === objectID);
 
-const addItem = (params) => {
-    const prevBasket = JSON.parse(localStorage.getItem(basket));
-    localStorage.setItem(basket, JSON.stringify([ ...prevBasket, params ]));
+    if (index === -1) {
+        const widget = {
+            name,
+            objectID,
+            count: 1,
+        };
+        localStorage.setItem(basket, JSON.stringify([ ...prevBasket, widget ]));
+    } else {
+        prevBasket[index].count++;
+        localStorage.setItem(basket, JSON.stringify(prevBasket));
+    }
 };
 
 const openBasket = () => {
+    const parent = document.getElementById(basket);
+    document.getElementsByClassName(basket + '-mask')[0].style.visibility = 'visible';
 
+    JSON.parse(localStorage.getItem(basket))?.forEach((item, index) => {
+       const element = document.createElement('div');
+       element.innerText = `${index + 1}) ${item.objectID} - ` + item.name + ` ${item.count}шт.`;
+       parent.appendChild(element);
+    });
 };
+
+document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27) {
+        document.getElementsByClassName(basket + '-mask')[0].style.visibility = 'hidden';
+        document.getElementById(basket).innerHTML = '';
+    }
+});
